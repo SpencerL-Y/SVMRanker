4234ac5965ed8582a2e3e6c52b0dc704dda281d8
lixie <lixie15@mails.ucas.ac.cn>
2020-06-20 19:27:57 +0800

    current

--- src/OneLoop.py
+++ src/OneLoop.py
@@ -1,3 +1,14 @@
 from z3 import *
 
-L = 
+L = [
+lambda x :(True and (x[0] > 0)),
+lambda x :[(x[0] + x[1]), (x[1] - 1), ],
+2,
+0,
+lambda x :[( x[0]+ x[1]), ( x[1]- 1), ],
+lambda x :(And( True, ( x[0]> 0))),
+False,
+]
+T = [
+[1, 0, 0], 
+[0, 1, 0]]
